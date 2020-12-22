0348541a307d7fa6e6595097592a9dbb3cb9f05b
cboswel1 <christianboswell86@gmail.com>
2020-12-21 17:43:17 -0700

    finishing folder structure for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-
 import './App.css';
+// import NavBar from "./components/NavBar/navBar";
+import { BrowserRouter as Router} from "react-router-dom";
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      {/* <NavBar /> */}
       <h1>Hello from App!</h1>
     </div>
+    </ Router>
   );
 }
 
