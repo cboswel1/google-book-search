82f8575cd58773620fa66d1def3f558788cc0f44
cboswel1 <christianboswell86@gmail.com>
2020-12-27 12:16:35 -0700

    heroku deploy

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,12 +6,15 @@ export default {
   searchBooks: function (book) {
     return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book);
   },
+  //save
   saveBook: function (bookData) {
     return axios.post("/api/books", bookData);
   },
+  //find
   savedBooks: function () {
     return axios.get("/api/books");
   },
+  //delete
   deleteBook: function (id) {
     return axios.delete("/api/books/" + id);
   },
